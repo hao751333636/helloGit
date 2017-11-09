93c31c497a640c0f4e6c9cf5104810af8bc1c5d5
hao751333636 <hao751333636@qq.com>
2017-11-09 22:13:31 +0800

    Update Main.java

--- hellogit/src/hellogit/Main.java
+++ hellogit/src/hellogit/Main.java
@@ -3,7 +3,10 @@ package hellogit;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello git!");
+		System.out.println("hello git5");
+		System.out.println("hello git6");
+		System.out.println("hello git7");
+		System.out.println("hello git8");
 	}
 
 }

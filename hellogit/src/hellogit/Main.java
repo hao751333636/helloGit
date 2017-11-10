9c61ad906805b4dc607b867cc1a82268d548c2d7
shi <hao751333636@qq.com>
2017-11-10 15:10:25 +0800

    commit

--- hellogit/src/hellogit/Main.java
+++ hellogit/src/hellogit/Main.java
@@ -3,6 +3,7 @@ package hellogit;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello git 1");
+		System.out.println("hello git 3");
+		System.out.println("hello git 4");
 	}
 }

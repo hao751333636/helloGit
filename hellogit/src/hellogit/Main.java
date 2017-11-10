615e19f2370432ad77135894220d0154bb6956e1
shi <hao751333636@qq.com>
2017-11-10 09:27:04 +0800

    'aaa'

--- hellogit/src/hellogit/Main.java
+++ hellogit/src/hellogit/Main.java
@@ -3,12 +3,6 @@ package hellogit;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello git1");
-		System.out.println("hello git2");
-		System.out.println("hello git3");
-		System.out.println("hello git4");
-		System.out.println("hello git5!!!!!");
-		System.out.println("hello git6!!!!!");
-		System.out.println("hello git7!!!!!");
+		System.out.println("hello git");
 	}
 }

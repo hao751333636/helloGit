89d7349fb399fd7c3c58d2cf29ed6e0f258871f7
shi <hao751333636@qq.com>
2017-11-10 09:14:16 +0800

    ""

--- hellogit/src/hellogit/Main.java
+++ hellogit/src/hellogit/Main.java
@@ -3,7 +3,10 @@ package hellogit;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello git1!!!!!");
+		System.out.println("hello git1");
+		System.out.println("hello git2");
+		System.out.println("hello git3");
+		System.out.println("hello git4");
 	}
 
 }
